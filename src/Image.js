36ad29f3a43b00e0ac6f0905e5be0146e7afe3ca
Dana Ciocan <danaciocan@economist.com>
2020-09-17 08:17:51 +0100

    Create basic hard-coded image element

---
+++ src/Image.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Image() {
+  return (
+    <div className="Image">
+      <img src="https://media.giphy.com/media/12HZukMBlutpoQ/giphy.gif" />
+    </div>
+  );
+}
+
+export default Image;
