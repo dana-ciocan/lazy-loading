4e54455ef10bac09a0c4aa445539d1a675a70ac2
Dana Ciocan <danaciocan@economist.com>
2020-09-17 08:19:24 +0100

    Add alt attribute to Image component

--- src/Image.js
+++ src/Image.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Image() {
   return (
     <div className="Image">
-      <img src="https://media.giphy.com/media/12HZukMBlutpoQ/giphy.gif" />
+      <img src="https://media.giphy.com/media/12HZukMBlutpoQ/giphy.gif" alt="It is a cat" />
     </div>
   );
 }
