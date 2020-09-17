515f2262bb15ab665c882d4eae96e99079a79dc3
Dana Ciocan <danaciocan@economist.com>
2020-09-17 12:29:48 +0100

    Apply formatting updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import ImageLoader from './ImageLoader'
 
 function App() {
   const offset = Math.floor(Math.random() * 100);
-  const topics = ['adventure time', 'steven universe', 'mario', 'nintendo', 'animal crossing', 'waiting', 'time', 'cat', 'dog', 'coffee', 'hungry'];
+  const topics = [
+    'adventure time',
+    'steven universe',
+    'mario',
+    'nintendo',
+    'animal crossing',
+    'waiting',
+    'time',
+    'cat',
+    'dog',
+    'coffee',
+    'hungry'
+  ];
   const randomTopic = topics[Math.floor(Math.random() * topics.length)];
   return (
     <div className="App">
