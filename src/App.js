4a3631c4e5c16e67802f0e297553dfcfb9adb5e0
Dana Ciocan <danaciocan@economist.com>
2020-09-18 14:19:42 +0100

    Add method selector

--- src/App.js
+++ src/App.js
@@ -4,23 +4,33 @@ import ImageLoader from './ImageLoader';
 
 function App() {
   const [topic, setTopic] = useState('');
+  const [method, setMethod] = useState('native');
   const [showImages, setShowImages] = useState(false);
   return (
     <div className="App">
       <div className="topic-form">
-        <label htmlFor="topic">Topic</label>
-        <input
-          type="text"
-          id="topic"
-          value={topic}
-          onChange={e => { setTopic(e.target.value); setShowImages(false); }}
-        />
+        <div className="form-row">
+          <label htmlFor="topic">Topic</label>
+          <input
+            type="text"
+            id="topic"
+            value={topic}
+            onChange={e => { setTopic(e.target.value); setShowImages(false); }}
+          />
+        </div>
+        <div className="form-row">
+        <label htmlFor="topic">Method</label>
+        <select onChange={e => setMethod(e.target.value)}>
+          <option value="native">Select one</option> 
+          <option value="native">Native lazy-loading</option> 
+        </select>
         <button onClick={() => setShowImages(true)}>OK</button>
+        </div>
       </div>
       {
         showImages ? 
           <ImageLoader
-          method="native"
+          method
           query={topic}
           num={50}
           offset={Math.floor(Math.random() * 100)}
