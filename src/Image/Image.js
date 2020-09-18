b4c1bf795660fed5f935fd7d549b3382adc011b0
Dana Ciocan <danaciocan@economist.com>
2020-09-18 16:27:34 +0100

    Load images normally if within viewport

--- src/Image/Image.js
+++ src/Image/Image.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 function Image({ image: { url, height, width }, text, method, defaultHeight }) {
-  const imageProps = {
-    alt: text,
-    height: height || defaultHeight || 400,
-    width
-  };
-  switch (method) {
-    case 'native':
-      imageProps.loading = 'lazy';
-      imageProps.src = url;
-      break;
-    case 'javascript':
-      imageProps['data-src'] = url;
-      imageProps.className = 'lazy';
-      break;
-    default:
-      imageProps.src = url;
-      break;
+  const curImage = useRef(null);
+  const calcProps = () => {
+    curImage.current.alt = text;
+    curImage.current.height = height || defaultHeight || 400;
+    curImage.current.width = width;
+
+    const scrollTop = window.pageYOffset;
+    const imageInViewport = curImage.current.offsetTop < (window.innerHeight + scrollTop)
+    const curMethod = imageInViewport ? 'none' : method;
+
+    switch (curMethod) {
+      case 'native':
+        curImage.current.loading = 'lazy';
+        curImage.current.src = url;
+        break;
+      case 'javascript':
+        curImage.current['data-src'] = url;
+        curImage.current.className = 'lazy';
+        break;
+      default:
+        curImage.current.src = url;
+        break;
+    }
   }
+
   return (
-      <img alt="" {...imageProps} />
+    <img
+      src="placeholder.gif"
+      ref={curImage}
+      alt=""
+      onError={calcProps}
+    />
   );
 }
 
