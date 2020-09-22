21f78716bc63ba36a435681d602a40bf583eac0d
Dana Ciocan <danaciocan@economist.com>
2020-09-22 20:21:06 +0100

    Move API call to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import SearchForm from './components/SearchForm/SearchForm';
 import PlainImageContainer from './ImageContainers/Plain';
@@ -14,53 +14,31 @@ function App() {
   const [topic, setTopic] = useState('');
   const [method, setMethod] = useState('none');
   const [showImages, setShowImages] = useState(false);
-  const doSetup = () => {
-    const lazyloadImages = document.querySelectorAll("img.lazy");
-    if(method === 'events') {
-      let lazyloadThrottleTimeout;
-      
-      const lazyload = () => {
-        if(lazyloadThrottleTimeout) {
-          clearTimeout(lazyloadThrottleTimeout);
-        }    
-        
-        lazyloadThrottleTimeout = setTimeout(function() {
-            const scrollTop = window.pageYOffset;
-            lazyloadImages.forEach((img) => {
-              if(img.offsetTop < (window.innerHeight + scrollTop)) {
-                  img.src = img.dataset.src;
-                  img.classList.remove('lazy');
-                }
-            });
-            if(lazyloadImages.length === 0) { 
-              document.removeEventListener("scroll", lazyload);
-              window.removeEventListener("resize", lazyload);
-              window.removeEventListener("orientationChange", lazyload);
-            }
-        }, 20);
-      }
-      
-      document.addEventListener("scroll", lazyload);
-      window.addEventListener("resize", lazyload);
-      window.addEventListener("orientationChange", lazyload);
-    } else if (method === 'api') {
-      const imageObserver = new IntersectionObserver((entries, observer) => {
-        entries.forEach(function(entry) {
-          if (entry.isIntersecting) {
-            const image = entry.target;
-            image.src = image.dataset.src;
-            image.classList.remove("lazy");
-            imageObserver.unobserve(image);
-          }
+  const [imagesToDisplay, setImagesToDisplay] = useState([]);
+  useEffect(() => {
+    const offset = Math.floor(Math.random() * 100);
+    const fetchData = async () => {
+      const url = `http://api.giphy.com/v1/gifs/search?api_key=zvj5cMK94bGXSPqbLIQo2GiTJXSpUEI5&q=${topic}&limit=50&rating=g&offset=${offset}`;
+      await fetch(url)
+        .then(response => response.json())
+        .then(response => {
+          const imageData = response.data.map(imageData => {
+            const { url, height, width } = imageData.images.original;
+            return {
+              url,
+              height,
+              width,
+              altText: `This is a GIPHY for ${topic}`,
+              className: 'image__visible'
+            };
+          });
+          setImagesToDisplay(imageData);
         });
-      });
-
-      lazyloadImages.forEach((image) => {
-        imageObserver.observe(image);
-      });
     }
-    setShowImages(true);
-  }
+    if (showImages) {
+      fetchData();
+    }
+  }, [topic, showImages]);
   return (
     <div className="app">
       <SearchForm
@@ -69,7 +47,7 @@ function App() {
         setMethod={setMethod}
         setShowImages={setShowImages}
         methodLookup={methodLookup}
-          />
+      />
       {showImages && <h2>GIPHYs for {topic} using '{methodLookup[method]}'</h2>}
       {showImages && method === 'none' && <PlainImageContainer imagesToDisplay={imagesToDisplay} />}
       {showImages && method === 'native' && <NativeImageContainer imagesToDisplay={imagesToDisplay} />}
