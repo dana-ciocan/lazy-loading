ef51f43f4e8afbb76895a3b07c9e4364625c0a1a
Dana Ciocan <danaciocan@economist.com>
2020-09-23 10:06:08 +0100

    Add blank placeholder

--- src/ImageContainers/LazySizes.js
+++ src/ImageContainers/LazySizes.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './ImageContainers.css';
 import 'lazysizes';
+import placeholder from './placeholder.gif';
 
 function LazySizesContainer({ imagesToDisplay }) {
   return (
     <div className="image-container">
-        {imagesToDisplay && imagesToDisplay.map((image) => {
+        {imagesToDisplay && imagesToDisplay.map((image, index) => {
             return <img 
                 alt={image.altText}
                 className={`${image.className} lazyload`}
+                src={index < 3 ? image.url : placeholder}
                 data-src={image.url}
                 width={image.width}
                 height={image.height}
