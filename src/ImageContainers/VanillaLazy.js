11f7be5c03c79452730f10eef8d2293539ef5cdf
Dana Ciocan <danaciocan@economist.com>
2020-09-23 09:37:11 +0100

    Tidy up vanilla lazy-loading implementation

--- src/ImageContainers/VanillaLazy.js
+++ src/ImageContainers/VanillaLazy.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './ImageContainers.css';
-import LazyImage from "./LazyImage";
+import LazyLoad from "vanilla-lazyload";
 
 function VanillaLazyloadingImageContainer({ imagesToDisplay }) {
+  useEffect(() => {
+    if (!document.lazyLoadInstance) {
+      document.lazyLoadInstance = new LazyLoad({ elements_selector: '.lazy' });
+    }
+    document.lazyLoadInstance.update();
+  });
   return (
     <div className="image-container">
         {imagesToDisplay && imagesToDisplay.map((image) => {
-            return <LazyImage
+            return <img 
                 alt={image.altText}
+                className="lazy"
                 data-src={image.url}
-                // data-srcset={}
-                // data-sizes={}
-                key={image.url}
-                height={image.height}
                 width={image.width}
+                height={image.height}
+                key={image.url}
             />;
         })}
     </div>
